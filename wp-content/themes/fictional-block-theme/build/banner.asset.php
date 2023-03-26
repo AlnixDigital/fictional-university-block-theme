d37f55571972b7f136e54107c08049beb6b7fb04
alejosilvalau <alejo.lautaro@hotmail.com>
2023-03-26 09:23:20 -0300

    Finishing banner block

---
+++ wp-content/themes/fictional-block-theme/build/banner.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '065a868700d14ac3e567');
---
+++ wp-content/themes/fictional-block-theme/build/genericbutton.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-primitives'), 'version' => '7d800c6a41a494a6ad7e');
